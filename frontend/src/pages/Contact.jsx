b0242576eb48399ef5e8e612a19f9f210cc1309f
Abu Fazal <abufazal460@gmail.com>
2026-07-04 20:30:40 +0530

    Add backgournds folder with spacebackground.jsx file add

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import SpaceBackground from '../components/backgrounds/SpaceBackground'
 
 const Contact = () => {
   return (
-    <div className='h-screen w-full text-center text-4xl'>Contact</div>
+    <div className='h-screen w-full text-center text-4xl'>
+      <SpaceBackground />
+    </div>
   )
 }
 
