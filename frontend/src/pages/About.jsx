9c124846a8f437794b0a3dd2405a9afc2e4df957
Abu Fazal <abufazal460@gmail.com>
2026-07-04 20:02:02 +0530

    home video changed

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='h-screen w-full text-center text-4xl'>About</div>
+    <div className='h-screen w-full text-center text-4xl bg-[#e2f0ff]'>About</div>
   )
 }
 
