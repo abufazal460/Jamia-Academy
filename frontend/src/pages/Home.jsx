28aacac8733526141492238b8b45eaa8dc76dc1d
Abu Fazal <abufazal460@gmail.com>
2026-07-08 13:03:44 +0530

    fix style in home pages

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,8 +3,8 @@ import FeatureSection from "../components/home/FeatureSection";
 
 const Home = () => {
   return (
-    <main className="h-screen w-full">
-      <img className="w-full h-full object-cover" src="hero-1.jpg" alt="" />
+    <main className="h-full w-full">
+      <img className="w-full h-screen object-cover" src="hero-1.jpg" alt="" />
 
       {/* Hero ke turant neeche — Section 01 */}
       <AccreditationSection />
