b85bb7b228814290053b2471b847d9d800eed815
Abu Fazal <abufazal460@gmail.com>
2026-07-08 20:11:55 +0530

    install some packages and add some file for packages and upadate home.jsx file code

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,34 @@
 import AccreditationSection from "../components/home/AccreditationSection";
 import FeatureSection from "../components/home/FeatureSection";
+import LiquidEther from "../components/home/LiquidEther";
 
 const Home = () => {
   return (
     <main className="h-full w-full">
-      <img className="w-full h-screen object-cover" src="hero-1.jpg" alt="" />
+      {/* <img className="w-full h-screen object-cover" src="hero-1.jpg" alt="" /> */}
+      
+      <div style={{ width: '100%', height: "100vh", position: 'relative', background: `url("hero-1.jpg") `}}>
+  <LiquidEther
+    colors={[ '#ffffff', '#ffffff', '#ffffff' ]}
+    mouseForce={20}
+    cursorSize={100}
+    isViscous
+    viscous={30}
+    iterationsViscous={32}
+    iterationsPoisson={32}
+    resolution={0.5}
+    isBounce={false}
+    autoDemo
+    autoSpeed={0.5}
+    autoIntensity={2.2}
+    takeoverDuration={0.25}
+    autoResumeDelay={3000}
+    autoRampDuration={0.6}
+    color0="#5227FF"
+    color1="#FF9FFC"
+    color2="#B497CF"
+/>
+</div>
 
       {/* Hero ke turant neeche — Section 01 */}
       <AccreditationSection />
