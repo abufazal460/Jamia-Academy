ee82da04b41a7834de0682bc518892c57646668f
Abu Fazal <abufazal460@gmail.com>
2026-07-11 20:07:54 +0530

    change minor style in gallery section and add public fonder insdie fonts folder with fonts file like inter poppins etc.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,35 +1,42 @@
 import AccreditationSection from "../components/home/AccreditationSection";
 import FeatureSection from "../components/home/FeatureSection";
 import LiquidEther from "../components/home/LiquidEther";
-import FAQ from "../components/faq/FAQ"
+import FAQ from "../components/faq/FAQ";
 
 const Home = () => {
   return (
     <main className="h-full w-full">
       {/* <img className="w-full h-screen object-cover" src="hero-1.jpg" alt="" /> */}
-      
-      <div style={{ width: '100%', height: "100vh", position: 'relative', background: `url("hero-1.jpg") `}}>
-  <LiquidEther
-    colors={[ '#ffffff', '#ffffff', '#ffffff' ]}
-    mouseForce={20}
-    cursorSize={100}
-    isViscous
-    viscous={30}
-    iterationsViscous={32}
-    iterationsPoisson={32}
-    resolution={0.5}
-    isBounce={false}
-    autoDemo
-    autoSpeed={0.5}
-    autoIntensity={2.2}
-    takeoverDuration={0.25}
-    autoResumeDelay={3000}
-    autoRampDuration={0.6}
-    color0="#5227FF"
-    color1="#FF9FFC"
-    color2="#B497CF"
-/>
-</div>
+
+      <div
+        style={{
+          width: "100%",
+          height: "100vh",
+          position: "relative",
+          background: `url(https://cdn.prod.website-files.com/696f172efacb311f86007ec0/69784fed37d6652a77f2bd2a_Background.svg)`,
+        }}
+      >
+        <LiquidEther
+          colors={["#ffffff", "#ffffff", "#ffffff"]}
+          mouseForce={20}
+          cursorSize={100}
+          isViscous
+          viscous={30}
+          iterationsViscous={32}
+          iterationsPoisson={32}
+          resolution={0.5}
+          isBounce={false}
+          autoDemo
+          autoSpeed={0.5}
+          autoIntensity={2.2}
+          takeoverDuration={0.25}
+          autoResumeDelay={3000}
+          autoRampDuration={0.6}
+          color0="#5227FF"
+          color1="#FF9FFC"
+          color2="#B497CF"
+        />
+      </div>
 
       {/* Hero ke turant neeche — Section 01 */}
       <AccreditationSection />
