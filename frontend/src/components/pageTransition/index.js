bd3282c90ba3a55514c26e48608cd72dc8e343bc
Abu Fazal <abufazal460@gmail.com>
2026-07-22 18:13:40 +0530

    add index.js file with code

---
+++ frontend/src/components/pageTransition/index.js
@@ -0,0 +1,7 @@
+export { default as PageTransition } from "./PageTransition";
+export { default as PageTransitionProvider } from "./PageTransitionProvider";
+export { default as TransitionOutlet } from "./TransitionOutlet";
+export { default as TransitionLink } from "./TransitionLink";
+export { default as TransitionContext } from "./TransitionContext";
+export { default as usePageTransition } from "./usePageTransition";
+export * from "./transitionPaths";
