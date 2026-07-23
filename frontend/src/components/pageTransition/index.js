0f9fa789bdc7d8bb22b5da7a760bcef988c70f32
Abu Fazal <abufazal460@gmail.com>
2026-07-23 13:25:27 +0530

    upadata index.js file and navItems.jsx file change 2 line code navlink remove add  TransitionNavLink

--- frontend/src/components/pageTransition/index.js
+++ frontend/src/components/pageTransition/index.js
@@ -2,6 +2,7 @@ export { default as PageTransition } from "./PageTransition";
 export { default as PageTransitionProvider } from "./PageTransitionProvider";
 export { default as TransitionOutlet } from "./TransitionOutlet";
 export { default as TransitionLink } from "./TransitionLink";
+export { default as TransitionNavLink } from "./TransitionNavLink";
 export { default as RouteTransitionWatcher } from "./RouteTransitionWatcher";
 export { default as TransitionContext } from "./TransitionContext";
 export { default as usePageTransition } from "./usePageTransition";
