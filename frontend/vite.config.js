8ef343dc5e72973b76684725288ae265d842f171
Abu Fazal <abufazal460@gmail.com>
2026-07-25 14:27:07 +0530

    update vite.config.js file and remove glsl import and code remove

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import glsl from "vite-plugin-glsl";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss(), glsl({
-      include: [
-        "**/*.glsl",
-        "**/*.vert",
-        "**/*.frag",
-      ],
-      warnDuplicatedImports: true,
-    }),],
+  plugins: [react() , tailwindcss()],
 });
