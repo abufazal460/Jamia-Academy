8503b746b4bc70e24ddef69bffc82bddcbfd2606
Abu Fazal <abufazal460@gmail.com>
2026-08-01 19:02:40 +0530

    hero/HeroTypewriterSubtitle.jsx file add with code

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,7 @@
 import AccreditationSection from "../components/home/AccreditationSection/AccreditationSection";
 import FeatureSection from "../components/home/FeatureSection/FeatureSection";
-import LiquidEther from "../components/home/hero/LiquidEther";
 import FAQ from "../components/faq/FAQ";
 import CourseGallerySection from "../components/home/CourseGallery/CourseGallerySection";
-import TextPressure from "../components/home/TextPressure";
 import Hero from "../components/home/hero/Hero";
 
 const Home = () => {
