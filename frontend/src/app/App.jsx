eb1cfda2ffe173ec278d006a51ce1c0475c12db6
Abu Fazal <abufazal460@gmail.com>
2026-08-17 20:46:24 +0530

    change location app.jsx file src/App.jsx to src/app/App.jsx and main.jsx fix import path app.jsx

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,22 +1,22 @@
 import React, { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/navigation/Navbar";
-import Footer from "./components/footer/Footer";
-import { IntroLoader, useAppReady } from "./App/ui/intro-loader";
+import Navbar from "../components/navigation/Navbar";
+import Footer from "../components/footer/Footer";
+import { IntroLoader, useAppReady } from "./ui/intro-loader";
 import {
   PageTransitionProvider,
   RouteTransitionWatcher,
-} from "./components/pageTransition";
-import SmoothScroll from "./components/smoothScroll/SmoothScroll";
-import OrganizationSchema from "./components/seo/OrganizationSchema";
+} from "../components/pageTransition";
+import SmoothScroll from "../components/smoothScroll/SmoothScroll";
+import OrganizationSchema from "../components/seo/OrganizationSchema";
 
-const HomePage = lazy(() => import("./pages/HomePage"));
-const AboutPage = lazy(() => import("./pages/AboutPage"));
-const CoursesPage = lazy(() => import("./pages/CoursesPage"));
-const GalleryPage = lazy(() => import("./pages/GalleryPage"));
-const ContactPage = lazy(() => import("./pages/ContactPage"));
-const LoginPage = lazy(() => import("./pages/LoginPage"));
-const CertificatePage = lazy(() => import("./pages/CertificatePage"));
+const HomePage = lazy(() => import("../pages/HomePage"));
+const AboutPage = lazy(() => import("../pages/AboutPage"));
+const CoursesPage = lazy(() => import("../pages/CoursesPage"));
+const GalleryPage = lazy(() => import("../pages/GalleryPage"));
+const ContactPage = lazy(() => import("../pages/ContactPage"));
+const LoginPage = lazy(() => import("../pages/LoginPage"));
+const CertificatePage = lazy(() => import("../pages/CertificatePage"));
 
 export default function App() {
   const appReady = useAppReady(); // ye batata hai site load ho gayi ya nahi
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./style/global.css";
-import App from "./App.jsx";
+import App from "./app/App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 
