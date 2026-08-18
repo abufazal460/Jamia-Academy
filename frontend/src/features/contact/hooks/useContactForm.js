8d5223371073731d52898fbae95097e5a4137e3a
Abu Fazal <abufazal460@gmail.com>
2026-08-18 13:03:53 +0530

    hooks folder remove file and add contact/hooks/useContactOfrm.js file add

--- frontend/src/features/contact/hooks/useContactForm.js
+++ frontend/src/features/contact/hooks/useContactForm.js
@@ -1,6 +1,6 @@
 import { useState, useCallback, useRef, useEffect } from "react";
 import { formConfig } from "../data/contactData";
-import { validateField } from "../utils/validators";
+import { validateField } from "../../../utils/validators";
 
 const buildInitialState = () => {
   const state = {};
