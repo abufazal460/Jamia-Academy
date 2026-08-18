57eecb70fbe7e826a934de2b9c4845d78900ebd6
Abu Fazal <abufazal460@gmail.com>
2026-08-18 10:32:31 +0530

    import name fixed in HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import AccreditationSection from "../features/home/components/accreditations/Acc
 import FeatureSection from "../features/home/components/features/FeatureSection";
 import FAQ from "../components/faq/FAQ";
 import CourseGallerySection from "../features/home/components/CourseGallery/CourseGallerySection";
-import WhyChooseUs from "../features/home/components/WhyChooseUs/WhyChooseUs";
+import WhyChooseUs from "../features/home/components/whyChooseUs/WhyChooseUs";
 import Testimonials from "../features/home/components/testimonials/Testimonials";
 
 const PAGE_TITLE = "Jamia Academy | Computer, Web Development, AI & Skill Courses in Delhi";
