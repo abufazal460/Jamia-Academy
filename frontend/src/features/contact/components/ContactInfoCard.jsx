888774fa7c90f7ca723518cff03f272b5a190fa0
Abu Fazal <abufazal460@gmail.com>
2026-08-18 13:28:33 +0530

    src/components/contact folder loction change features/contact/components - all file and delete contact folder

--- frontend/src/features/contact/components/ContactInfoCard.jsx
+++ frontend/src/features/contact/components/ContactInfoCard.jsx
@@ -1,9 +1,9 @@
 import { memo } from "react";
 import { motion } from "motion/react";
 import { Copy, Check, Phone, MessageCircle, Navigation } from "lucide-react";
-import { interactiveCardVariant, iconHoverVariant } from "../../animations/hoverVariants";
-import { resolveIcon } from "../../utils/iconResolver";
-import { useCopyToClipboard } from "../../hooks/useCopyToClipboard";
+import { interactiveCardVariant, iconHoverVariant } from "../../../../animations/hoverVariants";
+import { resolveIcon } from "../../../../utils/iconResolver";
+import { useCopyToClipboard } from "../../../../hooks/useCopyToClipboard";
 
 const ACTION_ICON = {
   call: Phone,
