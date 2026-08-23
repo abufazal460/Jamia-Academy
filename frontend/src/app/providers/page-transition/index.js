c72e9bd0be3fe0dd7c6e4268e24fbe8b963c8dc9
Abu Fazal <abufazal460@gmail.com>
2026-08-23 13:04:58 +0530

    remove import path in index.js file pagetransiton folder remove TrasitonOutlet.jsx file import path remove in index.js file

--- frontend/src/app/providers/page-transition/index.js
+++ frontend/src/app/providers/page-transition/index.js
@@ -1,6 +1,5 @@
 export { default as PageTransition } from "./PageTransition";
 export { default as PageTransitionProvider } from "./PageTransitionProvider";
-export { default as TransitionOutlet } from "./TransitionOutlet";
 export { default as TransitionLink } from "./TransitionLink";
 export { default as TransitionNavLink } from "./TransitionNavLink";
 export { default as RouteTransitionWatcher } from "./RouteTransitionWatcher";
