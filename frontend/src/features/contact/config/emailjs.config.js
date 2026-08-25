f52990f5611d26a94701063cca48f02fc53f5bbc
Abu Fazal <abufazal460@gmail.com>
2026-08-25 13:17:57 +0530

    properly working email js inquiry form submit without any show error and validation add in all input field like email phone and working proplery and gitignore file remove config folder and config/emailjs.config.js file remove in gitignore file all sensetive data add in .env file

---
+++ frontend/src/features/contact/config/emailjs.config.js
@@ -0,0 +1,6 @@
+// EmailJS credentials — sirf yahin se aate hain, kahin hardcode nahi
+export const emailjsConfig = {
+  serviceId: import.meta.env.VITE_SERVICE_ID,
+  templateId: import.meta.env.VITE_TEMPLATE_ID,
+  publicKey: import.meta.env.VITE_PUBLIC_KEY,
+};
