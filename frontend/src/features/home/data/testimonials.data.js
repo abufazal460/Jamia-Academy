6e4f5cbced8dae69615f8961ee0517c59ddafb10
Abu Fazal <abufazal460@gmail.com>
2026-09-01 17:28:51 +0530

    testimonials section complete

--- frontend/src/features/home/data/testimonials.data.js
+++ frontend/src/features/home/data/testimonials.data.js
@@ -11,7 +11,7 @@ import id9 from "../../../assets/images/home/student/Musab ACC.png";
 import id10 from "../../../assets/images/home/student/Reshma Parveen.png";
 import id11 from "../../../assets/images/home/student/samra.jpeg";
 import id12 from "../../../assets/images/home/student/shifa.png";
-// 13 mera hai abu fazal pending  
+import id13 from "../../../assets/images/home/student/fazal.png";
 import id14 from "../../../assets/images/home/student/Kaif ADFA.png";
 import id15 from "../../../assets/images/home/student/Parvez Alam ACC.png";
 import id16 from "../../../assets/images/home/student/MD_Anas_Interior_Design.png";
@@ -138,7 +138,7 @@ export const testimonialsRowTwo = [
     rating: 5,
     review:
       "I knew a little coding before joining Jamia Academy, but I couldn’t build projects on my own. After completing the Full Stack Development course, I’ve grown from struggling with basic projects to confidently building complex, real-world applications.",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqE3e9RIK09-ejpr7BwnvNb4TpTjmXHGgrTTNc00vdtA&s=10",
+    image: id13,
   },
   {
     id: 14,
