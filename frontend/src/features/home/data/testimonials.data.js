66bba69cda642f37982f9c84c741e4c00c521d70
Abu Fazal <abufazal460@gmail.com>
2026-09-01 13:47:58 +0530

    add more testimonils images and add almost complete description and images

--- frontend/src/features/home/data/testimonials.data.js
+++ frontend/src/features/home/data/testimonials.data.js
@@ -1,20 +1,32 @@
+import id1 from "../../../assets/images/home/student/Taiba_Khanam_ACC.png";
 import id2 from "../../../assets/images/home/student/Aasif Python.png";
+import id3 from "../../../assets/images/home/student/Hamza nafees BCC.jpeg";
+import id4 from "../../../assets/images/home/student/Saif Hussain Interior Design.png";
+import id5 from "../../../assets/images/home/student/Mohd Zaid Tally Prime.jpeg";
 import id6 from "../../../assets/images/home/student/Alfisha Digital Marketing.png";
 import id7 from "../../../assets/images/home/student/MD Muztar AUTOCAD.jpeg";
+import id8 from "../../../assets/images/home/student/noorhan.png";
+
 import id9 from "../../../assets/images/home/student/Musab ACC.png";
+import id10 from "../../../assets/images/home/student/Reshma Parveen.png";
 import id11 from "../../../assets/images/home/student/samra.jpeg";
 import id12 from "../../../assets/images/home/student/shifa.png";
+// 13 mera hai abu fazal pending  
+import id14 from "../../../assets/images/home/student/Kaif ADFA.png";
+import id15 from "../../../assets/images/home/student/Parvez Alam ACC.png";
+import id16 from "../../../assets/images/home/student/MD_Anas_Interior_Design.png";
+
 
 // Top row (left direction) ke liye data
 export const testimonialsRowOne = [
   {
     id: 1,
-    name: "Raiehah Noor",
-    course: "Data Analytics",
+    name: "Taiba Khanam",
+    course: "ACC (Advanced Computer Course)",
     rating: 5,
     review:
-      "I learned Excel and Power BI at Jamia Academy, and the teachers explained everything in a very simple way. I could understand even the hard topics easily. The teachers are always ready to answer questions, no matter how basic.",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHQ3V29WLjT5GB2D7afLWVSshBhLM_FjlHMoLyTmIAHQ&s=10",
+      "I learned Excel and Ms Word and Power Point at Jamia Academy, and the teachers explained everything in a very simple way. I could understand even the hard topics easily. The teachers are always ready to answer questions, no matter how basic.",
+    image: id1,
   },
   {
     id: 2,
@@ -27,30 +39,30 @@ export const testimonialsRowOne = [
   },
   {
     id: 3,
-    name: "Shahena",
-    course: "Data Analytics",
+    name: "Hamza nafees",
+    course: "BCC (Basic Computer Course)",
     rating: 4.8,
     review:
-      "Placement support kamaal ka hai. Mujhe graduation ke 2 mahine baad hi ek achhi company me job mil gayi.",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaFeioPLnEOiRJqeXk9mWZsfINuCow3ipsvFW3GBggpA&s=10",
+      "I had never used a computer before joining Jamia Academy. I enrolled in the BCC course, learned everything from the basics, and now I'm confident and skilled in using computers.",
+    image: id3,
   },
   {
     id: 4,
-    name: "Aman Khan, Sumbul",
+    name: "Saif Hussain",
     course: "Interior Design",
     rating: 4,
     review:
       "I learned how to plan spaces and choose colours properly. The teacher has real work experience and shares many useful tips. The classroom setup is nice and gives you space to be creative",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU8TFJ7iUwyhF0_LOmPpst5aFLBQUYvRcuREn63JTVvg&s=10",
+    image: id4,
   },
   {
     id: 5,
-    name: "Noor Ahmad",
+    name: "Mohd Zaid",
     course: "DFA (Tally)",
     rating: 4.3,
     review:
       "Accounting used to feel difficult, but the teachers explained it with easy examples. Now I understand the basics well. The overall environment at the academy is calm and helps you focus.",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqE3e9RIK09-ejpr7BwnvNb4TpTjmXHGgrTTNc00vdtA&s=10",
+    image: id5,
   },
   {
     id: 6,
@@ -72,12 +84,12 @@ export const testimonialsRowOne = [
   },
   {
     id: 8,
-    name: "Nasreen Masood",
-    course: "Revit CAD",
-    rating: 3.9,
+    name: "Noorhan Ahmad",
+    course: "Full Stack Web Development",
+    rating: 4.8,
     review:
-      "Revit felt tough in the beginning, but the teachers broke it down into easy steps. They showed real building models, which helped me understand 3D design better. The academy's computer lab is well set up for practice.",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6BkUjFsxfyWWDLXMSl9tD5p0nItjsRAVeww79SBYWNA&s=10",
+      "I joined the Full Stack Development course at Jamia Academy with very little coding knowledge. The teachers were supportive and explained things in a simple way. I got to work on real projects, and now I feel much more confident in my development skills.",
+    image: id8,
   },
 ];
 
@@ -94,12 +106,12 @@ export const testimonialsRowTwo = [
   },
   {
     id: 10,
-    name: "Hasan",
+    name: "Reshma Parveen",
     course: "BCC (Basic Computer Course)",
     rating: 5,
     review:
       "I didn't know much about computers before joining BCC. The teachers started from the basics and made sure everyone understood before moving ahead. Now I feel comfortable using a computer for daily work.",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6BkUjFsxfyWWDLXMSl9tD5p0nItjsRAVeww79SBYWNA&s=10",
+    image: id10,
   },
   {
     id: 11,
@@ -122,38 +134,38 @@ export const testimonialsRowTwo = [
   {
     id: 13,
     name: "Abu Fazal",
-    course: "Full Stack Development",
+    course: "Full Stack Web Development",
     rating: 5,
     review:
-      "Learning Tally here was simple and practical. Teachers showed real business examples, not just software. Now I can handle basic accounts confidently.",
+      "I knew a little coding before joining Jamia Academy, but I couldn’t build projects on my own. After completing the Full Stack Development course, I’ve grown from struggling with basic projects to confidently building complex, real-world applications.",
     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqE3e9RIK09-ejpr7BwnvNb4TpTjmXHGgrTTNc00vdtA&s=10",
   },
   {
     id: 14,
-    name: "dummy 1",
-    course: "BCC (Basic Computer Course)",
+    name: "Kaif",
+    course: "ADFA",
     rating: 4.8,
     review:
-      "Learning Tally here was simple and practical. Teachers showed real business examples, not just software. Now I can handle basic accounts confidently.",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQ01GPnlPxgeBdk7cU_gfuD2mrxfaLtHgcy5WwfQg13g&s=10",
+      "I joined the ADFA course at Jamia Academy with basic accounting knowledge. The practical training helped me understand accounting better, and now I can confidently manage real-world accounting tasks.",
+    image: id14,
   },
   {
     id: 15,
-    name: "dummy 2",
-    course: "BCC (Basic Computer Course)",
+    name: "Parvez Alam",
+    course: "ACC (Advanced Computer Course)",
     rating: 4.5,
     review:
-      "Learning Tally here was simple and practical. Teachers showed real business examples, not just software. Now I can handle basic accounts confidently.",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi_D-HUxY_L9H6fQfsdKAfn8yriVgzPMx36HQXz9-Bng&s=10",
+      "I learned computer basics properly at Jamia Academy. The teachers are patient and explain everything clearly, even if you are a complete beginner. Now I use what I learned every day.",
+    image: id15,
   },
   {
     id: 16,
-    name: "dummy 3",
-    course: "Tally Prime",
+    name: "MD Anas",
+    course: "Interior Design",
     rating: 5,
     review:
-      "Learning Tally here was simple and practical. Teachers showed real business examples, not just software. Now I can handle basic accounts confidently.",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHzgIvj_tKuzbc5yaALqcWzplgTmsGHD2t4CW7WwYDVw&s=10",
+      "I joined the Interior Design course at Jamia Academy with no professional experience. The practical training helped me understand design better, and now I can confidently create and work on real-world interior projects.",
+    image: id16,
   },
  
 ];
