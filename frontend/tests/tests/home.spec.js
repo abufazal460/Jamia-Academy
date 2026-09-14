b32b525799f6e28cfdf25402183eb78ed50c912e
Abu Fazal <abufazal460@gmail.com>
2026-09-14 14:14:26 +0530

    completely delete playwrites in entire project all packages delete in my packages.json file and fronted folder delete all folder and file

--- frontend/tests/tests/home.spec.js
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("Homepage loads successfully", async ({ page }) => {
-  await page.goto("http://localhost:5173");
-
-  await expect(page).toHaveTitle(/Jamia Academy/i);
-
-  await expect(page.locator("body")).toBeVisible();
-});
