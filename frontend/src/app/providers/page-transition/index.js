94b8e6979f17510c73da7ae9032090419acedcc6
Abu Fazal <abufazal460@gmail.com>
2026-09-16 15:15:25 +0530

    complete folder and all file delete in src/app/providers/page-transiton folder and all file delete

--- frontend/src/app/providers/page-transition/index.js
+++
@@ -1,8 +0,0 @@
-export { default as PageTransition } from "./PageTransition";
-export { default as PageTransitionProvider } from "./PageTransitionProvider";
-export { default as TransitionLink } from "./TransitionLink";
-export { default as TransitionNavLink } from "./TransitionNavLink";
-export { default as RouteTransitionWatcher } from "./RouteTransitionWatcher";
-export { default as TransitionContext } from "./TransitionContext";
-export { default as usePageTransition } from "./usePageTransition";
-export * from "./transitionPaths";
