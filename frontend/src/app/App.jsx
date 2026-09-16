c45095024adc4c6bd2b6589d6c3e7900a5a15d39
Abu Fazal <abufazal460@gmail.com>
2026-09-16 15:17:02 +0530

    App.jsx file code remove page transtion code remove and useless comment remove

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -2,11 +2,6 @@ import React, { Suspense, lazy } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "../shared/components/navigation/Navbar";
 import Footer from "../shared/components/footer/Footer";
-// import { IntroLoader, useAppReady } from "./ui/intro-loader";
-import {
-  PageTransitionProvider,
-  RouteTransitionWatcher,
-} from "../app/providers/page-transition";
 import SmoothScroll from "./providers/SmoothScroll";
 import OrganizationSchema from "../shared/seo/OrganizationSchema";
 import Layout from "../shared/components/layout/Layout";
@@ -23,37 +18,32 @@ const CertificatePage = lazy(() => import("../pages/CertificatePage"));
 const NotFoundPage = lazy(() => import("../pages/NotFoundPage"));
 
 export default function App() {
-  // const appReady = useAppReady();
   const location = useLocation();
 
   return (
     <>
       <OrganizationSchema />
-      {/* <IntroLoader appReady={appReady}> */}
-        <PageTransitionProvider>
-          <SmoothScroll>
-            <Suspense fallback={null}>
-              <Navbar />
-              <RouteTransitionWatcher />
-              <Layout>
-                <ErrorBoundary resetKeys={[location.pathname, location.search]}>
-                  <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/about" element={<AboutPage />} />
-                    <Route path="/course" element={<CoursesPage />} />
-                    <Route path="/gallery" element={<GalleryPage />} />
-                    <Route path="/contact" element={<ContactPage />} />
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/certificate" element={<CertificatePage />} />
-                    <Route path="*" element={<NotFoundPage />} />
-                  </Routes>
-                </ErrorBoundary>
-              </Layout>
-              <Footer />
-            </Suspense>
-          </SmoothScroll>
-        </PageTransitionProvider>
-      {/* </IntroLoader> */}
+      <SmoothScroll>
+        <Suspense fallback={null}>
+          <Navbar />
+          <RouteTransitionWatcher />
+          <Layout>
+            <ErrorBoundary resetKeys={[location.pathname, location.search]}>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/course" element={<CoursesPage />} />
+                <Route path="/gallery" element={<GalleryPage />} />
+                <Route path="/contact" element={<ContactPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/certificate" element={<CertificatePage />} />
+                <Route path="*" element={<NotFoundPage />} />
+              </Routes>
+            </ErrorBoundary>
+          </Layout>
+          <Footer />
+        </Suspense>
+      </SmoothScroll>
     </>
   );
 }
